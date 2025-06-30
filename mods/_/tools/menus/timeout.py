5ee3f78f737bc7e485e891e82d7ef3c0f0c9b121
anamoyee <thecreatorrr333@gmail.com>
2025-06-30 16:40:39 +0200

    Fix the bug where the wrong message would be edited when menu times out

--- mods/_/tools/menus/timeout.py
+++ mods/_/tools/menus/timeout.py
@@ -1,14 +1,40 @@
-from miru.ext.menu import Menu
+from miru.ext.menu import Menu, ScreenContent
 
 
-class ClearItemsOnTimeoutMenu(Menu):
+class _BruteForceFixTimeoutEditMenu(Menu):
+	"""Fix the bug, where on_timeout() will edit any ctx.respond() response message and not the first message, which breaks on_timeout()."""
+
+	def _client_start_hook(self, *args, **kwargs) -> None:
+		if self.is_bound is None:
+			raise RuntimeError("Cannot start an unbound _BruteForceFixTimeoutEditMenu, add bound_to= keyword argument to miru.Client().start_view(...).")
+
+		super()._client_start_hook(*args, **kwargs)
+
+	async def on_timeout(self) -> None:  # replicate the original on_timeout() but with self.update_original_message() instead
+		for item in self.children:
+			item.disabled = True
+
+		await self.update_original_message()
+
+	async def update_original_message(self, new_content: ScreenContent | None = None) -> None:
+		"""Update always the first, bound, as opposed to the ctx.respond()-ed, message, which is not what .update_message() does."""
+		if self.message is None:
+			raise RuntimeError("Unable to find original message to update on an unbound (unstarted) Menu.")
+
+		if new_content is not None:
+			self._payload = new_content._build_payload()
+
+		await self.message.edit(components=self, **self._payload)
+
+
+class ClearItemsOnTimeoutMenu(_BruteForceFixTimeoutEditMenu):
 	async def on_timeout(self) -> None:
 		self.clear_items()
 
 		return await super().on_timeout()
 
 
-class DisableItemsOnTimeoutMenu(Menu):
+class DisableItemsOnTimeoutMenu(_BruteForceFixTimeoutEditMenu):
 	def disable_items(self) -> None:
 		for item in self.children:
 			item.disabled = True
